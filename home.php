d9aeb7bc8c1c036b14c5fc335097eb9af61c4dac
Jayadevi Pineiro gomez <jpineiro@c3r4s1.42madrid.com>
2022-06-13 19:41:42 +0200

    actualizacionPages

--- home.php
+++ home.php
@@ -2,8 +2,32 @@
 require_once 'header.php';
 ?>
 
-
-
+<!--<div class="card mb-3">
+    <img src="images/logo_transitdolls.jpg" class="card-img-top" alt="imgShop">
+    <div class="card-body">
+        <h5 class="card-title">Tienda</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+</div>-->
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="images/logo_transitdolls.jpg" class="img-fluid rounded-start" alt="imgShop">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Tienda</h5>
+        <p class="card-text">Tienda online para coleccionistas de muñecas.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class="card mb-3">
+    <h2>Blog</h2>
+</div>
 <?php
 require_once 'footer.php';
 ?>
